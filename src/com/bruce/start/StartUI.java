0664f3a9e68de1c0ba7293f38962fea4493fd315
Bruce <ismyblue@163.com>
2017-10-22 12:12:43 +0800

    finish

--- src/com/bruce/start/StartUI.java
+++ src/com/bruce/start/StartUI.java
@@ -1,7 +1,6 @@
 package com.bruce.start;
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 import com.bruce.model.Task;
 
@@ -21,75 +20,154 @@ public class StartUI {
 				+ "４）查询任务．" + "５）查看所有任务．" + "６）退出．\n";
 	}
 
+	// 添加任务
+	public void addTask() {
+		String name, id, desc, comments;
+
+		userInput.asking("请输入名称：");
+		do {
+			name = scanner.next();
+		} while (name.equals(""));
+
+		userInput.asking("请输入任务类型:system/user：");
+		do {
+			id = scanner.next();
+		} while (id.equals(""));
+
+		userInput.asking("请输入任务描述：");
+		do {
+			desc = scanner.next();
+		} while (desc.equals(""));
+
+		userInput.asking("请输入任务备注：");
+		do {
+			comments = scanner.next();
+		} while (comments.equals(""));
+
+		Task t = new Task(name, id, desc, comments, Tracker.getPosition());
+		if (myTracker.addItem(t))
+			userInput.asking("添加成功！\n");
+		else
+			userInput.asking("没有添加成功！\n");
+
+	}
+
+	// 删除任务
 	public void deleteTask() {
-		this.myTracker.deleteAllItem();
+		userInput.asking("请输入程序当前运行号：\n");
+		int cur;
+		while (true) {
+			if (scanner.hasNextInt()) {
+				cur = scanner.nextInt();
+				break;
+			}
+		}
+		if (myTracker.deleteItem(myTracker.findByCurrent(cur)))
+			userInput.asking("删除成功！");
+		else
+			userInput.asking("没有删除成功!");
+	}
+
+	// 更新任务
+	public void updateTask() {
+		userInput.asking("请输入程序当前运行号：\n");
+		int cur;
+		while (true) {
+			if (scanner.hasNextInt()) {
+				cur = scanner.nextInt();
+				break;
+			}
+		}
+
+		String name, id, desc, comments;
+
+		userInput.asking("请输入名称：");
+		do {
+			name = scanner.next();
+		} while (name.equals(""));
+
+		userInput.asking("请输入任务类型:system/user：");
+		do {
+			id = scanner.next();
+		} while (id.equals(""));
+
+		userInput.asking("请输入任务描述：");
+		do {
+			desc = scanner.next();
+		} while (desc.equals(""));
+
+		userInput.asking("请输入任务备注：");
+		do {
+			comments = scanner.next();
+		} while (comments.equals(""));
+
+		Task t = new Task(name, id, desc, comments, cur);
+		if (myTracker.updateItem(t))
+			userInput.asking("更新成功！");
+		else
+			userInput.asking("没有更新成功!");
+	}
+
+	// 查询任务
+	public void queryTask() {
+		String ask = "1)通过id查询：\n2)通过任务当前运行号查询:\n3)通过id和name查询:\n";
+		String id, name;
+		int cur;
+		userInput.asking(ask);
+		int an = scanner.nextInt();
+		switch (an) {
+		case 1:
+			userInput.asking("输入任务id:\n");
+			id = scanner.next();
+			myTracker.displayItems(myTracker.findById(id));
+			break;
+		case 2:
+			userInput.asking("输入任务当前运行号:\n");
+			cur = scanner.nextInt();
+			myTracker.displayItem(myTracker.findByCurrent(cur));
+			break;
+		case 3:
+			userInput.asking("输入id:\n");
+			id = scanner.next();
+			userInput.asking("输入name:\n");
+			name = scanner.next();
+			myTracker.displayItems(myTracker.findByFiler(id, name));
+			break;
+		default:
+			break;
+		}
 	}
 
 	public void printAllTask() {
 		this.myTracker.displayAllItem();
 	}
 
-	public void printByFilter() {
-
-	}
-
-	public void updateTask() {
-		
-	}
-	
-	public void addTask(){
-		String name,id,desc,comments;
-		
-		userInput.asking("请输入名称：");
-		do{
-			name = scanner.next();			
-			System.out.println("*" + name + "*");
-			System.out.println(name.getClass().getName());
-		}while(name.equals(""));
-		
-		userInput.asking("请输入任务类型:system/user：");		
-		do{
-			id = scanner.next();			
-		}while(id.equals(""));
-		
-		userInput.asking("请输入任务描述：");
-		do{
-			desc = scanner.next();
-			System.out.println(desc);
-		}while(desc.equals(""));
-
-		userInput.asking("请输入任务备注：");
-		do{
-			comments = scanner.next();
-		}while(comments.equals(""));
-		
-		Task t = new Task(name, id, desc, comments, Tracker.getPosition());
-		myTracker.addItem(t);
-	}
-
 	public void Run() {
 		boolean isClose = false;
 		while (true) {
-			userInput.asking(menu);			
-			while(!scanner.hasNextInt()){
+			userInput.asking(menu);
+			while (!scanner.hasNextInt()) {
 				scanner.next();
 			}
-			int an = scanner.nextInt();			
+			int an = scanner.nextInt();
 			switch (an) {
-			case 1: {//增加
-				addTask();				
+			case 1: {// 增加
+				addTask();
 				break;
 			}
-			case 2: {
+			case 2: {// 删除
+				deleteTask();
 				break;
 			}
-			case 3: {
+			case 3: {// 修改
+				updateTask();
 				break;
 			}
-			case 4: {
+			case 4: {// 查询
+				queryTask();
 				break;
 			}
-			case 5: {
+			case 5: {// 查看所有
 				myTracker.displayAllItem();
 				break;
 			}
@@ -107,7 +185,7 @@ public class StartUI {
 	}
 
 	public static void main(String[] args) {
-		
+
 		StartUI desktop = new StartUI();
 		desktop.init();
 		desktop.Run();
--- src/com/bruce/start/Tracker.java
+++ src/com/bruce/start/Tracker.java
@@ -1,5 +1,6 @@
 package com.bruce.start;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -7,7 +8,7 @@ import com.bruce.model.*;
 
 public class Tracker {
 	// 保存当前创建了任务的最后一个标识号码，current
-	private static int position = 0;
+	private static int position = 1;
 	// 任务管理器当前管理的所有任务
 	private ArrayList<Task> allTask;
 
@@ -24,18 +25,24 @@ public class Tracker {
 	}
 
 	// 增加一个运行任务
-	public void addItem(Task task) {
-		allTask.add(task);
-		setPosition(getPosition() + 1);// 增加标号
+	public boolean addItem(Task task) {
+		if(allTask.add(task)){
+			setPosition(getPosition() + 1);// 增加标号
+			return true;
+		}
+		return false;
+		
 	}
 
 	// 删除/关闭一个任务
-	public void deleteItem(Task task) {
-		allTask.remove(task);
+	public boolean deleteItem(Task task) {
+		if(allTask.remove(task))
+			return true;
+		return false;
 	}
 
 	public void deleteAllItem() {
-		this.allTask.clear();
+		allTask.clear();		
 	}
 
 	// 搜索运行中的任务 @param: String id , String name
@@ -51,8 +58,8 @@ public class Tracker {
 		Task[] r = new Task[list.size()];
 		list.toArray(r);
 		return r;
-	}
-
+	}	
+	
 	// 查找任务 @param: String id
 	public Task[] findById(String id) {
 		ArrayList<Task> list = new ArrayList<Task>();
@@ -66,34 +73,63 @@ public class Tracker {
 		return r;
 	}
 
-	// 更新task
-	public void updateItem(Task task) {
-		for (int i = 0; i < allTask.size(); i++) {
-			if (task.getCurrent() == allTask.get(i).getCurrent()) {
-				allTask.set(i, task);
+	// 查找任务 @param: int current
+	public Task findByCurrent(int cur) {
+		for (Task t : allTask) {
+			if (t.getCurrent() == cur) {
+				return t;
 			}
 		}
+		return null;
+	}
+
+	// 更新task
+	public boolean updateItem(Task task) {
+		for (int i = 0; i < allTask.size(); i++) {
+			if (task.getCurrent() == allTask.get(i).getCurrent()) {
+				allTask.get(i).setName(task.getName());
+				allTask.get(i).setCreate(task.getCreate());
+				allTask.get(i).setDescription(task.getDescription());
+				allTask.get(i).setId(task.getId());
+				allTask.get(i).setComment(task.getComment());
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void displayItem(Task task) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");		
+		System.out.println("---------------------------------");
+		System.out.println("任务当前运行号Current:	" + task.getCurrent());
+		System.out.println("任务名称Name:		" + task.getName());
+		System.out.println("创建时间Create:		" + format.format(task.getCreate()));
+		System.out.println("任务类型id:		" + task.getId());
+		System.out.println("描述Description:		" + task.getDescription());
+		System.out.println("备注Comment:		" + task.getComment());
+		
 	}
 
 	public void displayItems(Task[] taskList) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");		
 		for (int i = 0; i < taskList.length; i++) {
 			System.out.println("---------------------------------");
 			System.out.println("任务当前运行号Current:	" + taskList[i].getCurrent());
 			System.out.println("任务名称Name:		" + taskList[i].getName());
-			System.out.println("创建时间Create:		" + taskList[i].getCreate());
+			System.out.println("创建时间Create:		" + format.format(taskList[i].getCreate()));
 			System.out.println("任务类型id:		" + taskList[i].getId());
-			System.out.println("描述Description:		"
-					+ taskList[i].getDescription());
+			System.out.println("描述Description:		" + taskList[i].getDescription());
 			System.out.println("备注Comment:		" + taskList[i].getComment());
 		}
 	}
 
 	public void displayAllItem() {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");		
 		for (Task t : allTask) {
 			System.out.println("---------------------------------");
 			System.out.println("任务当前运行号Current:	" + t.getCurrent());
 			System.out.println("任务名称Name:		" + t.getName());
-			System.out.println("创建时间Create:		" + t.getCreate());
+			System.out.println("创建时间Create:		" + format.format(t.getCreate()));
 			System.out.println("任务类型id:		" + t.getId());
 			System.out.println("描述Description:		" + t.getDescription());
 			System.out.println("备注Comment:		" + t.getComment());
